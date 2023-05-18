ec7ebb701d7c4a46a3391f36b13a64b57198ff6c
McEw3965 <EWAN.MCCARNEY22@bathspa.ac.uk>
2023-05-18 20:34:41 +0100

    Adds circle variables to js

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ let slide2 = document.getElementById("slide2");
 let slide3 = document.getElementById("slide3");
 let image = document.getElementById("image");
 
-
-
-document.addEventListener("visibilitychange", function () {
+document.addEventListener("visibilitychange", function() {
 
     if (document.hidden) {
         slide1.style.animationPlayState = "paused";
@@ -17,3 +15,10 @@ document.addEventListener("visibilitychange", function () {
         slide3.style.animationPlayState = "running";
     }
 } );
+
+const circle1 = document.getElementById("circle1");
+const circle2 = document.getElementById("circle2");
+const circle3 = document.getElementById("circle3");
+const circle4 = document.getElementById("circle4");
+const circle5 = document.getElementById("circle5");
+
