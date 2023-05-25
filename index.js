0d7561b4b8b95dc4d3f37ed6f40e066e1ee328b6
McEw3965 <EWAN.MCCARNEY22@bathspa.ac.uk>
2023-05-25 14:59:12 +0100

    Adds function to change page to dark mode.
    Page switches to dark mode. Change does not persist between pages

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let slide2 = document.getElementById("slide2");
 let slide3 = document.getElementById("slide3");
 let image = document.getElementById("image");
 
-document.addEventListener("visibilitychange", function() {
+document.addEventListener("visibilitychange", function () {
 
     if (document.hidden) {
         slide1.style.animationPlayState = "paused";
@@ -14,5 +14,81 @@ document.addEventListener("visibilitychange", function() {
         slide2.style.animationPlayState = "running";
         slide3.style.animationPlayState = "running";
     }
-} );
+});
+
+
+const root = document.documentElement;
+const computedStyles = getComputedStyle(root);
+
+let isDarkMode;
+
+function darkModeToggle() {
+
+    console.log('A' + isDarkMode);
+
+    isDarkMode = parseInt(localStorage.getItem("isDarkMode"));
+
+    console.log('B' + isDarkMode);
+
+    if (isDarkMode === 0) {
+        document.documentElement.style.setProperty('--secondary-colour', '#fbf3f2');
+        document.documentElement.style.setProperty('--background-colour', '#032539');
+
+        const secondaryColour = computedStyles.getPropertyValue('--secondary-colour');
+        const backgroundColour = computedStyles.getPropertyValue('--background-colour');
+
+        console.log('C' + isDarkMode);
+        
+        // isDarkMode = 1; 
+
+        localStorage.setItem("isDarkMode", "1"); 
+
+    } else if (isDarkMode === 1) {
+        document.documentElement.style.setProperty('--secondary-colour', '#032539');
+        document.documentElement.style.setProperty('--background-colour', '#fbf3f2');
+
+        const secondaryColour = computedStyles.getPropertyValue('--secondary-colour');
+        const backgroundColour = computedStyles.getPropertyValue('--background-colour');
+
+        console.log('C' + isDarkMode);
+
+       // isDarkMode = 0;
+
+        localStorage.setItem("isDarkMode", "0");
+    }
+}
+
+function darkModeCheck() {
+
+    isDarkMode = parseInt(localStorage.getItem("isDarkMode"));
+
+    if (!isDarkMode) {
+        console.log('Is Running');
+        isDarkMode = 1;
+    }
+
+    console.log(isDarkMode);
+
+    if (isDarkMode === 1) {
+        document.documentElement.style.setProperty('--secondary-colour', '#fbf3f2');
+        document.documentElement.style.setProperty('--background-colour', '#032539');
+
+        const secondaryColour = computedStyles.getPropertyValue('--secondary-colour');
+        const backgroundColour = computedStyles.getPropertyValue('--background-colour');
+
+    } else if (isDarkMode === 0) {
+        document.documentElement.style.setProperty('--secondary-colour', '#032539');
+        document.documentElement.style.setProperty('--background-colour', '#fbf3f2');
+
+        const secondaryColour = computedStyles.getPropertyValue('--secondary-colour');
+        const backgroundColour = computedStyles.getPropertyValue('--background-colour');
+
+    }
+}
+
+window.addEventListener('load', function () {
+
+    darkModeCheck();
+})
+
 
