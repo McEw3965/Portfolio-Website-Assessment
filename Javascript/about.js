c25ad8bb3e51256f7683d90b301c4bf7bcdcc0d7
McEw3965 <EWAN.MCCARNEY22@bathspa.ac.uk>
2023-05-27 18:24:43 +0100

    Adss comments

--- Javascript/about.js
+++ Javascript/about.js
@@ -1,18 +1,16 @@
-let progressText = document.getElementsByClassName("progressText");
+let progressText = document.getElementsByClassName("progressText"); //Assigns each element of class to space in array
 
-
-
-window.addEventListener('load', function () {
+window.addEventListener('load', function () { //Triggers when page loads
 
     console.log(progressText);
 
-    setTimeout(function () {
-        document.getElementById("cPlusPlus").classList.remove("progressZero");
+    setTimeout(function () { //Sets a delay of 0 seconds. 
+        document.getElementById("cPlusPlus").classList.remove("progressZero"); //Removes class from element, allowing for smooth transition
     }, 0)
 
-    setTimeout(function () {
+    setTimeout(function () { //Sets a delay of 2 seconds. New transition triggers after previous one
         document.getElementById("html").classList.remove("progressZero");
-        progressText[0].style.opacity = "100%";
+        progressText[0].style.opacity = "100%"; // Sets opacity of text after delay
     }, 2000)
 
     setTimeout(function () {
@@ -26,7 +24,7 @@ window.addEventListener('load', function () {
     }, 6000)
 
     setTimeout(function () {
-        progressText[3].style.opacity = "100%";
+        progressText[3].style.opacity = "100%"; //Triggers after all other transitions
     }, 8000)
 
 })
--- Javascript/index.js
+++ Javascript/index.js
@@ -1,58 +1,48 @@
+//Initialises key variables
+
 let slide1 = document.getElementById("slide1");
 let slide2 = document.getElementById("slide2");
 let slide3 = document.getElementById("slide3");
-let image = document.getElementById("image");
 
-document.addEventListener("visibilitychange", function () {
+document.addEventListener("visibilitychange", function () { //Triggers if window visibility is changed
 
     if (document.hidden) {
         slide1.style.animationPlayState = "paused";
         slide2.style.animationPlayState = "paused";
-        slide3.style.animationPlayState = "paused";
+        slide3.style.animationPlayState = "paused"; //Pauses animation if window is hidden
     } else {
-        slide1.style.animationPlayState = "running";
+        slide1.style.animationPlayState = "running"; //Plays animation if window is visible
         slide2.style.animationPlayState = "running";
         slide3.style.animationPlayState = "running";
     }
 });
 
 
-const root = document.documentElement;
-const computedStyles = getComputedStyle(root);
+const root = document.documentElement; //Creates constant variable 'root', corresponding to the root element in css files
 
 isDarkMode = parseInt(localStorage.getItem("isDarkMode"));
 
-console.log('Before if:' + isDarkMode);
-
-if (isDarkMode != 1 & isDarkMode != 0) {
+if (isDarkMode != 1 & isDarkMode != 0) { //If isDarkMode is not equal to one or zero, set vairblae equal to 0
     isDarkMode = 0;
-    console.log('isRunning');
 }
 
-function darkModeToggle() {
+function darkModeToggle() { //Creates function
 
-    isDarkMode = parseInt(localStorage.getItem("isDarkMode"));
-
-    console.log(isDarkMode);
+    isDarkMode = parseInt(localStorage.getItem("isDarkMode")); //Sets value of isDarkMode equal to value in local storage. ParseInt ensure numeric value
 
     if (isDarkMode === 0) {
         document.documentElement.style.setProperty('--secondary-colour', '#fbf3f2');
-        document.documentElement.style.setProperty('--background-colour', '#032539');
+        document.documentElement.style.setProperty('--background-colour', '#032539'); //Sets value of css variables. Swaps background colour and text colour
 
-        const secondaryColour = computedStyles.getPropertyValue('--secondary-colour');
-        const backgroundColour = computedStyles.getPropertyValue('--background-colour');
+        document.getElementById("darkMode").innerHTML = "☼ Light Mode"; //Changes text in dark mode button
 
-        document.getElementById("darkMode").innerHTML = "☼ Light Mode";
-
-        localStorage.removeItem('isDarkMode');
-        localStorage.setItem("isDarkMode", "1");
+        localStorage.removeItem('isDarkMode'); //Removes value of variable in local storage
+        localStorage.setItem("isDarkMode", "1"); //Sets value of variable in local storage
 
     } else if (isDarkMode === 1) {
         document.documentElement.style.setProperty('--secondary-colour', '#032539');
         document.documentElement.style.setProperty('--background-colour', '#fbf3f2');
 
-        const secondaryColour = computedStyles.getPropertyValue('--secondary-colour');
-        const backgroundColour = computedStyles.getPropertyValue('--background-colour');
 
         document.getElementById("darkMode").innerHTML = "☼ Dark Mode";
 
@@ -61,14 +51,11 @@ function darkModeToggle() {
     }
 }
 
-function darkModeCheck() {
+function darkModeCheck() { //Similar function to above. Removes some features to allow darkmode to persist between screens
 
     if (isDarkMode === 1) {
         document.documentElement.style.setProperty('--secondary-colour', '#fbf3f2');
         document.documentElement.style.setProperty('--background-colour', '#032539');
-
-        const secondaryColour = computedStyles.getPropertyValue('--secondary-colour');
-        const backgroundColour = computedStyles.getPropertyValue('--background-colour');
 
         document.getElementById("darkMode").innerHTML = "☼ Light Mode";
 
@@ -76,47 +63,41 @@ function darkModeCheck() {
         document.documentElement.style.setProperty('--secondary-colour', '#032539');
         document.documentElement.style.setProperty('--background-colour', '#fbf3f2');
 
-        const secondaryColour = computedStyles.getPropertyValue('--secondary-colour');
-        const backgroundColour = computedStyles.getPropertyValue('--background-colour');
-
         document.getElementById("darkMode").innerHTML = "☼ Dark Mode";
 
     }
 }
 
-window.addEventListener('load', function () {
-
+window.addEventListener('load', function () { //Triggers function when window loads
     darkModeCheck();
 })
 
 //Text to Speech
 
-const heading1 = document.getElementsByTagName("h1");
+const heading1 = document.getElementsByTagName("h1"); //Defines heading1 variable
 
-const synth = window.speechSynthesis;
-let text = "Text to speech is working";
+const synth = window.speechSynthesis; //Creates synth variable
 
-let TTSActive = 0;
+let TTSActive = 0; //Creates variable for toggle
 
-function speak(text) {
-    const utterThis = new SpeechSynthesisUtterance(text);
-    synth.speak(utterThis);
+function speak(text) { //Function needs to be passed a value that will be assigned to 'text' variable found in function
+    const utterThis = new SpeechSynthesisUtterance(text); //Creates instance of text to speech
+    synth.speak(utterThis); //Reads out given text
 }
 
 function TTS() {
-    console.log('running');
-    const selectedText = window.getSelection().toString().trim();
-    if (selectedText !== '') {
+    const selectedText = window.getSelection().toString().trim(); //Gets text currently selected on page. Converts to a string. Removes whitespace at start and end of string
+    if (selectedText !== '') { /*If selected text is not equal to an empty space, read out selected text*/
         speak(selectedText);
     }
 }
 
 function activateTTS() {
-    if (TTSActive === 0) {
-        document.addEventListener('mouseup', TTS);
-        TTSActive = 1;
+    if (TTSActive === 0) { /*If TTSActive is qual to 0, execute following code*/
+        document.addEventListener('mouseup', TTS); /*Adds event listener that triggers TTS function when mouse is released*/
+        TTSActive = 1; /*Changes value of TTSActive variable to 1*/
     } else if (TTSActive === 1) {
-        document.removeEventListener('mouseup', TTS);
+        document.removeEventListener('mouseup', TTS); /*Removes evenet listener, allowing a toggle button*/
         TTSActive = 0;
     }
 }
